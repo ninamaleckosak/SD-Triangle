c1e733f9f52439f7bf23a66ff80a2b1075d506e7
ninamaleckosak <156948856+ninamaleckosak@users.noreply.github.com>
2024-01-24 12:37:55 -0500

    Update Triangle.rb

--- Triangle.rb
+++ Triangle.rb
@@ -1,2 +1,12 @@
 def triangle(x, y, z)
-  
+  if x = y
+    if y = z
+      puts "equilateral"
+    else
+      puts "isosceles"
+  elif y = z
+      puts "isosceles"
+  elif x = z
+      puts "isosceles"
+  else
+      puts "scalene"
